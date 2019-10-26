af7a69ad286af9f5d05206e825cec9fe5cc5b14f
Umer Shaikh <umershaikh305@gmail.com>
2019-10-26 23:22:42 +0500

    Redux stand-alone example in index.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Open console and see index.js for Redux example
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -2,11 +2,44 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+import { createStore } from 'redux';
+
+const initialState = {
+    todos: [],
+    posts: [],
+}
+
+const myReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'ADD_TODO':
+            return {
+                ...state,
+                todos: [...state.todos, action.payload],
+            }
+        case 'ADD_POSTS':
+            return {
+                ...state,
+                posts: [...state.posts, action.payload],
+            }
+        default:
+            return state;
+    }
+}
+
+const store = createStore(myReducer);
+
+store.subscribe(() => {    // This function will be called, whenever the state will be updated
+    console.log("State Updated : ", store.getState())
+});
+
+store.dispatch({ type: "ADD_TODO", payload: 'Programming' });
+store.dispatch({ type: "ADD_POSTS", payload: 'This is Post 1' });
+store.dispatch({ type: "ADD_TODO", payload: 'Go to university' });
+store.dispatch({ type: "ADD_POSTS", payload: 'This is Post 2' });
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
